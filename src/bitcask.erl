c3beadbeda7cebbf9f9c73956af94a72feff45ad
Dave Smith <dizzyd@dizzyd.com>
2010-03-28 00:45:51 -0600

    Implement write lock functionality. Provide hooks to pass options in when opening a bitcask.

--- src/bitcask.erl
+++ src/bitcask.erl
@@ -21,7 +21,7 @@
 %% -------------------------------------------------------------------
 -module(bitcask).
 
--export([open/1,
+-export([open/1, open/2,
          close/1,
          get/2,
          put/3,
@@ -37,33 +37,89 @@
 -record(bc_state, {dirname,
                    write_file,                  % File for writing
                    read_files,                  % Files opened for reading
-                   max_file_size = 16#80000000, % 2GB default max_file size
+                   max_file_size,               % Max. size of a written file
                    keydir}).                    % Key directory
 
 -define(TOMBSTONE, <<"bitcask_tombstone">>).
+-define(DEFAULT_MAX_FILE_SIZE, 16#80000000). % 2GB default
 
-%% Filename convention is {integer_timestamp}.bitcask
+%% A bitcask is a directory containing:
+%% * One or more data files - {integer_timestamp}.bitcask.data
+%% * A write lock - bitcask.write.lock (Optional)
+%% * A merge lock - bitcask.merge.lock (Optional)
 
 open(Dirname) ->
+    open(Dirname, []).
+
+open(Dirname, Opts) ->
     %% Make sure the directory exists
     ok = filelib:ensure_dir(filename:join(Dirname, "bitcask")),
 
+    %% If the read_write option is set, attempt to acquire the write lock file.
+    %% Do this first to avoid unnecessary processing of files for reading.
+    case proplists:get_bool(read_write, Opts) of
+        true ->
+            %% Try to acquire the write lock, or bail if unable to
+            case write_lock_acquire(Dirname) of
+                true ->
+                    %% Open up the new file for writing and update the write lock file
+                    {ok, NewWritingFile} = bitcask_fileops:create_file(Dirname),
+                    ok = write_lock_update(Dirname, bitcask_fileops:filename(NewWritingFile));
+                false ->
+                    NewWritingFile = undefined, % Make erlc happy w/ non-local exit
+                    throw({error, write_locked})
+            end;
+        false ->
+            NewWritingFile = undefined
+    end,
+
+    %% If we don't have a NewWritingFile, check the write lock and see
+    %% what file is currently active so we don't attempt to read it.
+    case NewWritingFile of
+        undefined ->
+            {_ActivePid, ActiveFile} = write_lock_check(Dirname);
+        _ ->
+            ActiveFile = undefined
+    end,
+
     %% Build a list of all the bitcask data files and sort it in
-    %% descending order (newest->oldest)
-    SortedFiles = list_data_files(Dirname),
+    %% descending order (newest->oldest). Pass the currently active
+    %% writing file so that one is not loaded (may be 'undefined')
+    SortedFiles = list_data_files(Dirname, ActiveFile),
 
     %% Setup a keydir and scan all the data files into it
     {ok, KeyDir} = bitcask_nifs:keydir_new(),
     ReadFiles = scan_key_files(SortedFiles, KeyDir, []),
-    {ok, WriteFile} = bitcask_fileops:create_file(Dirname),
+
+    %% Get the max file size parameter from opts
+    case proplists:get_value(max_file_size, Opts, ?DEFAULT_MAX_FILE_SIZE) of
+        MaxFileSize when is_integer(MaxFileSize) ->
+            ok;
+        _ ->
+            MaxFileSize = ?DEFAULT_MAX_FILE_SIZE
+    end,
+
+    %% Setup basic state
     {ok, #bc_state{dirname = Dirname,
-                   write_file = WriteFile,
                    read_files = ReadFiles,
+                   write_file = NewWritingFile, % May be undefined
+                   max_file_size = MaxFileSize,
                    keydir = KeyDir}}.
 
-close(#bc_state { write_file = WriteFile, read_files = ReadFiles }) ->
+
+close(#bc_state { write_file = WriteFile, read_files = ReadFiles, dirname = Dirname }) ->
+    %% Clean up all the reading files
     [ok = bitcask_fileops:close(F) || F <- ReadFiles],
-    ok = bitcask_fileops:close(WriteFile).
+
+    %% If we have a write file, assume we also currently own the write lock
+    %% and cleanup both of those
+    case WriteFile of
+        undefined ->
+            ok;
+        _ ->
+            ok = bitcask_fileops:close(WriteFile),
+            ok = file:delete(filename:join(Dirname, "bitcask.write.lock"))
+    end.
 
 
 get(#bc_state{keydir = KeyDir} = State, Key) ->
@@ -84,6 +140,8 @@ get(#bc_state{keydir = KeyDir} = State, Key) ->
             {error, Reason}
     end.
 
+put(#bc_state{ write_file = undefined }, _Key, _Value) ->
+    {error, read_only};
 put(#bc_state{ dirname = Dirname, keydir = KeyDir } = State, Key, Value) ->
     case bitcask_fileops:check_write(State#bc_state.write_file,
                                      Key, Value, State#bc_state.max_file_size) of
@@ -108,9 +166,8 @@ put(#bc_state{ dirname = Dirname, keydir = KeyDir } = State, Key, Value) ->
                                  Size, OffSet, Tstamp),
     {ok, State1#bc_state { write_file = NewWriteFile }}.
 
-delete(_State, _Key) ->
-    %put(State,Key,?TOMBSTONE).
-    ok.
+delete(State, Key) ->
+    put(State, Key, ?TOMBSTONE).
 
 
 %% ===================================================================
@@ -137,7 +194,7 @@ scan_key_files([Filename | Rest], KeyDir, Acc) ->
 
 
 get_filestate(FileId, #bc_state{ dirname = Dirname, read_files = ReadFiles } = State) ->
-    Fname = bitcask_fileops:filename(Dirname, FileId),
+    Fname = bitcask_fileops:mk_filename(Dirname, FileId),
     case lists:keysearch(Fname, #filestate.filename, ReadFiles) of
         {value, Filestate} ->
             {Filestate, State};
@@ -146,7 +203,7 @@ get_filestate(FileId, #bc_state{ dirname = Dirname, read_files = ReadFiles } = S
             {Filestate, State#bc_state { read_files = [Filestate | State#bc_state.read_files] }}
     end.
 
-list_data_files(Dirname) ->
+list_data_files(Dirname, WritingFile) ->
     %% Build a list of {tstamp, filename} for all files in the directory that
     %% match our regex. Then reverse sort that list and extract the fully-qualified
     %% filename.
@@ -154,23 +211,91 @@ list_data_files(Dirname) ->
                                fun(F, Acc) ->
                                        [{bitcask_fileops:file_tstamp(F), F} | Acc]
                                end, []),
-    [filename:join(Dirname, F) || {_Tstamp, F} <- reverse_sort(Files)].
+    [filename:join(Dirname, F) || {_Tstamp, F} <- reverse_sort(Files),
+                                  F /= WritingFile].
 
+write_lock_check(Dirname) ->
+    write_lock_check(Dirname, 3).
+
+write_lock_check(Dirname, 0) ->
+    error_logger:error_msg("Timed out waiting for partial write lock in ~s\n", [Dirname]),
+    {undefined, undefined};
+write_lock_check(Dirname, Count) ->
+    case file:read_file(filename:join(Dirname, "bitcask.write.lock")) of
+        {ok, Bin} ->
+            case re:run(Bin, "([0-9]+) (.*)\n", [{capture, all_but_first, list}]) of
+                {match, [WritingFile, []]} ->
+                    {WritingFile, undefined};
+                {match, [WritingPid, WritingFile]} ->
+                    {WritingPid, WritingFile};
+                nomatch ->
+                    %% A lock file exists, but is not complete.
+                    timer:sleep(10),
+                    write_lock_check(Dirname, Count-1)
+            end;
+        {error, enoent} ->
+            %% Lock file doesn't exist
+            {undefined, undefined};
+        {error, Reason} ->
+            error_logger:error_msg("Failed to check write lock in ~s: ~p\n", [Dirname, Reason]),
+            {undefined, undefined}
+    end.
+
+write_lock_acquire(Dirname) ->
+    case bitcask_nifs:create_file(filename:join(Dirname, "bitcask.write.lock")) of
+        true ->
+            %% Write out our PID w/ empty place for file name (since we don't know it yet)
+            ok = file:write_file(filename:join(Dirname, "bitcask.write.lock"),
+                                 [os:getpid(), " \n"]),
+            true;
+        false ->
+            false
+    end.
+
+write_lock_update(Dirname, ActiveFileName) ->
+    ThisOsPid = os:getpid(),
+    case write_lock_check(Dirname) of
+        {ThisOsPid, _} ->
+            ok = file:write_file(filename:join(Dirname, "bitcask.write.lock"),
+                                 [os:getpid(), " ", ActiveFileName, "\n"]);
+        _ ->
+            {error, not_write_lock_owner}
+    end.
 
 %% ===================================================================
 %% EUnit tests
 %% ===================================================================
 -ifdef(TEST).
 
+init_dataset(Dirname, KVs) ->
+    init_dataset(Dirname, [], KVs).
+
+init_dataset(Dirname, Opts, KVs) ->
+    os:cmd(?FMT("rm -rf ~s", [Dirname])),
+
+    {ok, B} = bitcask:open(Dirname, [read_write] ++ Opts),
+    lists:foldl(fun({K, V}, Bc0) ->
+                        {ok, Bc} = bitcask:put(Bc0, K, V),
+                        Bc
+                end, B, KVs).
+
+
+default_dataset() ->
+    [{<<"k">>, <<"v">>},
+     {<<"k2">>, <<"v2">>},
+     {<<"k3">>, <<"v3">>}].
+
+
 roundtrip_test() ->
     os:cmd("rm -rf /tmp/bc.test.roundtrip"),
-    {ok, B} = bitcask:open("/tmp/bc.test.roundtrip"),
+    {ok, B} = bitcask:open("/tmp/bc.test.roundtrip", [read_write]),
     {ok, B1} = bitcask:put(B,<<"k">>,<<"v">>),
     {ok, <<"v">>, B2} = bitcask:get(B1,<<"k">>),
     {ok, B3} = bitcask:put(B2, <<"k2">>, <<"v2">>),
     {ok, B4} = bitcask:put(B3, <<"k">>,<<"v3">>),
     {ok, <<"v2">>, B5} = bitcask:get(B4, <<"k2">>),
-    {ok, <<"v3">>, _} = bitcask:get(B5, <<"k">>).
+    {ok, <<"v3">>, B6} = bitcask:get(B5, <<"k">>),
+    close(B6).
 
 list_data_files_test() ->
     os:cmd("rm -rf /tmp/bc.test.list; mkdir -p /tmp/bc.test.list"),
@@ -183,76 +308,41 @@ list_data_files_test() ->
     [] = os:cmd(?FMT("touch ~s", [string:join(ExpFiles, " ")])),
 
     %% Now use the list_data_files to scan the dir
-    ExpFiles = list_data_files("/tmp/bc.test.list").
+    ExpFiles = list_data_files("/tmp/bc.test.list", undefined).
 
 fold_test() ->
-    os:cmd("rm -rf /tmp/bc.test.fold"),
+    B = init_dataset("/tmp/bc.test.fold", default_dataset()),
 
-    Inputs = [{<<"k">>, <<"v">>},
-              {<<"k2">>, <<"v2">>},
-              {<<"k3">>, <<"v3">>}],
-
-    {ok, B} = bitcask:open("/tmp/bc.test.fold"),
-    B1 = lists:foldl(fun({K, V}, Bc0) ->
-                             {ok, Bc} = bitcask:put(Bc0, K, V),
-                             Bc
-                     end, B, Inputs),
-
-    File = B1#bc_state.write_file,
+    File = B#bc_state.write_file,
     L = bitcask_fileops:fold(File, fun(K, V, _Ts, _Pos, Acc) ->
                                            [{K, V} | Acc]
                                    end, []),
-    Inputs = lists:reverse(L).
+    ?assertEqual(default_dataset(), lists:reverse(L)),
+    close(B).
 
 open_test() ->
-    os:cmd("rm -rf /tmp/bc.test.open"),
-
-    Inputs = [{<<"k">>, <<"v">>},
-              {<<"k2">>, <<"v2">>},
-              {<<"k3">>, <<"v3">>}],
+    close(init_dataset("/tmp/bc.test.open", default_dataset())),
 
     {ok, B} = bitcask:open("/tmp/bc.test.open"),
-    B1 = lists:foldl(fun({K, V}, Bc0) ->
-                             {ok, Bc} = bitcask:put(Bc0, K, V),
-                             Bc
-                     end, B, Inputs),
-
-    close(B1),
-
-    {ok, B2} = bitcask:open("/tmp/bc.test.open"),
     lists:foldl(fun({K, V}, Bc0) ->
                        {ok, V, Bc} = bitcask:get(Bc0, K),
                        Bc
-               end, B2, Inputs).
+               end, B, default_dataset()).
 
 wrap_test() ->
-    os:cmd("rm -rf /tmp/bc.test.wrap"),
-
-    Inputs = [{<<"k">>, <<"v">>},
-              {<<"k2">>, <<"v2">>},
-              {<<"k3">>, <<"v3">>}],
+    %% Initialize dataset with max_file_size set to 1 so that each file will
+    %% only contain a single key.
+    close(init_dataset("/tmp/bc.test.wrap", [{max_file_size, 1}], default_dataset())),
 
     {ok, B} = bitcask:open("/tmp/bc.test.wrap"),
 
-    %% Tweak max file size to be 1 byte so that every put should create a new
-    %% file
-    B1 = B#bc_state { max_file_size = 1 },
-    B2 = lists:foldl(fun({K, V}, Bc0) ->
-                             {ok, Bc} = bitcask:put(Bc0, K, V),
-                             Bc
-                     end, B1, Inputs),
-
-    close(B2),
-
-    {ok, B3} = bitcask:open("/tmp/bc.test.wrap"),
-
     %% Expect 4 files, since we open a new one on startup + the 3
     %% for each key written
-    4 = length(B3#bc_state.read_files),
+    4 = length(B#bc_state.read_files),
     lists:foldl(fun({K, V}, Bc0) ->
                        {ok, V, Bc} = bitcask:get(Bc0, K),
                        Bc
-               end, B3, Inputs).
+               end, B, default_dataset()).
 
 
 -endif.
