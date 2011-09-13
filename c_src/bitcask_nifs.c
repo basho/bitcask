a69c4bcd4ca7cd14a75ad958c24df70f4a616baf
Jon Meredith <jmeredith@basho.com>
2011-09-13 07:34:43 -0600

    Allow max_fold_age and max_fold_puts to be disabled independently.

--- c_src/bitcask_nifs.c
+++ c_src/bitcask_nifs.c
@@ -858,8 +858,9 @@ ERL_NIF_TERM bitcask_nifs_keydir_copy(ErlNifEnv* env, int argc, const ERL_NIF_TE
 // Check the number of updates since pending was created is less than the maximum
 // and that the current view is not too old.
 // Call with ts set to zero to force a wait on any pending keydir.
-// Set maxage and maxputs negative to force using the keydir - useful when 
-// a process has waited once and needs to run next time.
+// Set maxage or maxputs negative to ignore them.  Set both negative to force
+// using the keydir - useful when a process has waited once and needs to run 
+// next time.
 static int can_itr_keydir(bitcask_keydir* keydir, uint64_t ts, int maxage, int maxputs)
 {
     if (keydir->pending == NULL ||   // not frozen or caller wants to reuse 
@@ -874,7 +875,8 @@ static int can_itr_keydir(bitcask_keydir* keydir, uint64_t ts, int maxage, int m
     else
     {
         uint64_t age = ts - keydir->pending_start;
-        return (age <= maxage && keydir->pending_updated <= maxputs);
+        return ((maxage < 0 || age <= maxage) && 
+                (maxputs < 0 || keydir->pending_updated <= maxputs));
     }
 }
 
