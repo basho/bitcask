f20c14374b7f8d4cc1ab852dcaf22ff245d39aa4
Joseph Blomstedt <joe@basho.com>
2012-11-27 16:54:17 -0800

    Use Erlang file I/O from dedicated procs rather than NIFs
    Bitcask previously used raw file I/O to read/write files. However, since
    raw file I/O uses a non-optimized selective receive to wait for a reply
    back from the efile driver, this approach had numerous problems when
    Bitcask was used within processes with many incoming messages (such as how
    Bitcask is used in Riak).
    
    In commit 79d5eb3470db7691f4584004563c1b2f19eb0ad4, NIFs were introduced
    to solve this problem. The file I/O NIFs would block the Erlang scheduler,
    but solve the issue encountered with selective receive. Unfortunately,
    using blocking NIFs is much worse than originally thought. Thus, NIFs are
    not the right solution to this problem.
    
    This commit changes Bitcask to once again use Erlang's built-in file I/O,
    but now wraps each open file in a separate gen_server that interacts with
    the raw port. The original process now waits on a gen_server reply which
    uses an optimized selective receive, while the file process handles the
    unoptimized selective receive from the port driver. In our usage, the file
    process only has a single request outstanding, and therefore does not run
    into the selective receive issue.

---
+++ src/bitcask_file.erl
@@ -0,0 +1,136 @@
+-module(bitcask_file).
+-compile(export_all).
+-behaviour(gen_server).
+
+%% API
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         terminate/2, code_change/3]).
+
+-record(state, {fd}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+file_open(Filename, Opts) ->
+    {ok, Pid} = gen_server:start(?MODULE, [], []),
+    case gen_server:call(Pid, {file_open, Filename, Opts}, infinity) of
+        ok ->
+            {ok, Pid};
+        Error ->
+            Error
+    end.
+
+file_close(Pid) ->
+    file_request(Pid, file_close).
+
+file_sync(Pid) ->
+    file_request(Pid, file_sync).
+
+file_pread(Pid, Offset, Size) ->
+    file_request(Pid, {file_pread, Offset, Size}).
+
+file_pwrite(Pid, Offset, Bytes) ->
+    file_request(Pid, {file_pwrite, Offset, Bytes}).
+
+file_read(Pid, Size) ->
+    file_request(Pid, {file_read, Size}).
+
+file_write(Pid, Bytes) ->
+    file_request(Pid, {file_write, Bytes}).
+
+file_seekbof(Pid) ->
+    file_request(Pid, file_seekbof).
+
+%%%===================================================================
+%%% API helper functions
+%%%===================================================================
+
+file_request(Pid, Request) ->
+    case check_pid(Pid) of
+        ok ->
+            gen_server:call(Pid, Request, infinity);
+        Error ->
+            Error
+    end.
+
+check_pid(Pid) ->
+    IsPid = is_pid(Pid),
+    IsAlive = IsPid andalso is_process_alive(Pid),
+    case {IsAlive, IsPid} of
+        {true, _} ->
+            ok;
+        {false, true} ->
+            %% Same result as `file' module when accessing closed FD
+            {error, einval};
+        _ ->
+            %% Same result as `file' module when providing wrong arg
+            {error, badarg}
+    end.
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+init([]) ->
+    {ok, #state{}}.
+
+handle_call({file_open, Filename, Opts}, _From, State) ->
+    Mode = case proplists:get_bool(readonly, Opts) of
+               true ->
+                   [read, raw, binary, read_ahead];
+               false ->
+                   [read, write, raw, binary, read_ahead]
+           end,
+    %% [lager:warning("Option ~p ignored", [Opt]) || Opt <- [create, o_sync],
+    %%                                               proplists:get_bool(Opt, Opts)],
+    case file:open(Filename, Mode) of
+        {ok, Fd} ->
+            State2 = State#state{fd=Fd},
+            {reply, ok, State2};
+        Error ->
+            {reply, Error, State}
+    end;
+handle_call(file_close, _From, State=#state{fd=Fd}) ->
+    ok = file:close(Fd),
+    {stop, normal, ok, State};
+handle_call(file_sync, _From, State=#state{fd=Fd}) ->
+    Reply = file:sync(Fd),
+    {reply, Reply, State};
+handle_call({file_pread, Offset, Size}, _From, State=#state{fd=Fd}) ->
+    Reply = file:pread(Fd, Offset, Size),
+    {reply, Reply, State};
+handle_call({file_pwrite, Offset, Bytes}, _From, State=#state{fd=Fd}) ->
+    Reply = file:pwrite(Fd, Offset, Bytes),
+    {reply, Reply, State};
+handle_call({file_read, Size}, _From, State=#state{fd=Fd}) ->
+    Reply = file:read(Fd, Size),
+    {reply, Reply, State};
+handle_call({file_write, Bytes}, _From, State=#state{fd=Fd}) ->
+    Reply = file:write(Fd, Bytes),
+    {reply, Reply, State};
+handle_call(file_seekbof, _From, State=#state{fd=Fd}) ->
+    {ok, _} = file:position(Fd, bof),
+    {reply, ok, State};
+
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+terminate(_Reason, _State) ->
+    ok.
+
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
