basho/bitcask

commit 6c9717319b35b4234a6002bc435f8914b72b1524

avoid file_server_2 contention

src/bitcask.erl
@@ -474,9 +474,9 @@ merge(Dirname, Opts, {FilesToMerge0, ExpiredFiles0}) ->
     %% possible in some circumstances that we'll get an out-of-date
     %% list of files.
     FilesToMerge = [F || F <- FilesToMerge0,
-                         filelib:is_file(F)],
+                         bitcask_fileops:is_file(F)],
     ExpiredFiles = [F || F <- ExpiredFiles0,
-                         filelib:is_file(F)],
+                         bitcask_fileops:is_file(F)],
     merge1(Dirname, Opts, FilesToMerge, ExpiredFiles).
 
 %% Inner merge function, assumes that bitcask is running and all files exist.
@@ -634,7 +634,7 @@ needs_merge(Ref) ->
     %% previous merges). Close these files so that we don't leak
     %% file descriptors.
     P = fun(F) ->
-                filelib:is_file(bitcask_fileops:filename(F))
+                bitcask_fileops:is_file(bitcask_fileops:filename(F))
         end,
     {LiveFiles, DeadFiles} = lists:partition(P, State#bc_state.read_files),
 
@@ -653,7 +653,7 @@ needs_merge(Ref) ->
         {ok, 0} ->
             ok;
         {ok, Warn} ->
-            error_logger:info_msg("Trimmed ~p non-zero fstat entries",
+            error_logger:info_msg("Trimmed ~p non-existent fstat entries",
                                   [Warn]);
         Err ->
             error_logger:error_msg("Error trimming fstats entries: ~p",
@@ -822,7 +822,7 @@ summary_info(Ref) ->
 
     %% Remove any files that don't exist from the initial summary
     Summary = lists:keysort(1, [S || S <- Summary0,
-                                     filelib:is_file(element(2, S))]),
+                                     bitcask_fileops:is_file(element(2, S))]),
     {KeyCount, Summary}.
 
 
@@ -1099,7 +1099,8 @@ inner_merge_write(K, V, Tstamp, OldFileId, OldOffset, State) ->
                 %% Start our next file and update state
                 {ok, NewFile} = bitcask_fileops:create_file(
                                   State#mstate.dirname,
-                                  State#mstate.opts),
+                                  State#mstate.opts,
+                                  State#mstate.live_keydir),
                 NewFileName = bitcask_fileops:filename(NewFile),
                 ok = bitcask_lockops:write_activefile(
                        State#mstate.merge_lock,
@@ -1111,7 +1112,8 @@ inner_merge_write(K, V, Tstamp, OldFileId, OldOffset, State) ->
                 %% create the output file and take the lock.
                 {ok, NewFile} = bitcask_fileops:create_file(
                                   State#mstate.dirname,
-                                  State#mstate.opts),
+                                  State#mstate.opts,
+                                  State#mstate.live_keydir),
                 NewFileName = bitcask_fileops:filename(NewFile),
                 ok = bitcask_lockops:write_activefile(
                        State#mstate.merge_lock,
@@ -1231,7 +1233,8 @@ do_put(Key, Value, #bc_state{write_file = WriteFile} = State, Retries, _LastErr)
                 {ok, WriteLock} ->
                     {ok, NewWriteFile} = bitcask_fileops:create_file(
                                            State#bc_state.dirname,
-                                           State#bc_state.opts),
+                                           State#bc_state.opts,
+                                           State#bc_state.keydir),
                     ok = bitcask_lockops:write_activefile(
                            WriteLock,
                            bitcask_fileops:filename(NewWriteFile)),
@@ -1272,7 +1275,8 @@ wrap_write_file(#bc_state{write_file = WriteFile} = State) ->
     LastWriteFile = bitcask_fileops:close_for_writing(WriteFile),
     {ok, NewWriteFile} = bitcask_fileops:create_file(
                            State#bc_state.dirname,
-                           State#bc_state.opts),
+                           State#bc_state.opts,
+                           State#bc_state.keydir),
     ok = bitcask_lockops:write_activefile(
            State#bc_state.write_lock,
            bitcask_fileops:filename(NewWriteFile)),
@@ -1282,13 +1286,13 @@ wrap_write_file(#bc_state{write_file = WriteFile} = State) ->
 
 set_setuid_bit(File) ->
     %% We're intentionally opinionated about pattern matching here.
-    {ok, FI} = file:read_file_info(File),
+    {ok, FI} = bitcask_fileops:read_file_info(File),
     NewFI = FI#file_info{mode = FI#file_info.mode bor 8#4000},
-    ok = file:write_file_info(File, NewFI).
+    ok = bitcask_fileops:write_file_info(File, NewFI).
 
 has_setuid_bit(File) ->
     try
-        {ok, FI} = file:read_file_info(File),
+        {ok, FI} = bitcask_fileops:read_file_info(File),
         FI#file_info.mode band 8#4000 == 8#4000
     catch _:_ ->
             false

src/bitcask_fileops.erl
@@ -23,7 +23,7 @@
 %% @doc Basic file i/o operations for bitcask.
 -module(bitcask_fileops).
 
--export([create_file/2,
+-export([create_file/3,
          open_file/1,
          close/1,
          close_for_writing/1,
@@ -39,6 +39,7 @@
          hintfile_name/1,
          file_tstamp/1,
          check_write/4]).
+-export([read_file_info/1, write_file_info/2, is_file/1]).
 
 -include_lib("kernel/include/file.hrl").
 
@@ -61,10 +62,40 @@
 
 %% @doc Open a new file for writing.
 %% Called on a Dirname, will open a fresh file in that directory.
--spec create_file(Dirname :: string(), Opts :: [any()]) -> {ok, #filestate{}}.
-create_file(DirName, Opts) ->
-    create_file_loop(DirName, [create] ++ Opts, most_recent_tstamp(DirName) + 1).
-
+-spec create_file(Dirname :: string(), Opts :: [any()],
+                  reference()) -> 
+                         {ok, #filestate{}}.
+create_file(DirName, Opts, Keydir) ->
+    {ok, Lock} = bitcask_lockops:acquire(create, DirName),
+    try
+        {ok, Newest} = bitcask_nifs:increment_file_id(Keydir),
+        
+        Filename = mk_filename(DirName, Newest),
+        ok = ensure_dir(Filename),
+        
+        %% Check for o_sync strategy and add to opts
+        FinalOpts = 
+            case bitcask:get_opt(sync_strategy, Opts) of
+                o_sync ->
+                    [o_sync | Opts];
+                _ ->
+                    Opts
+            end,
+        
+        {ok, FD} = bitcask_io:file_open(Filename, FinalOpts),
+        HintFD = open_hint_file(Filename, FinalOpts),
+        {ok, #filestate{mode = read_write,
+                        filename = Filename,
+                        tstamp = file_tstamp(Filename),
+                        hintfd = HintFD, fd = FD, ofs = 0}}
+    catch Error:Reason ->
+            %% if we fail somehow, do we need to nuke any partial
+            %% state?
+            {Error, Reason}
+    after
+        bitcask_lockops:release(Lock)
+    end.
+        
 %% @doc Open an existing file for reading.
 %% Called with fully-qualified filename.
 -spec open_file(Filename :: string()) -> {ok, #filestate{}} | {error, any()}.
@@ -109,16 +140,27 @@ close_hintfile(State = #filestate { hintfd = HintFd, hintcrc = HintCRC }) ->
     bitcask_io:file_close(HintFd),
     State#filestate { hintfd = undefined, hintcrc = 0 }.
 
-
 %% Build a list of {tstamp, filename} for all files in the directory that
 %% match our regex. 
 -spec data_file_tstamps(Dirname :: string()) -> [{integer(), string()}].
 data_file_tstamps(Dirname) ->
-    filelib:fold_files(Dirname, "^[0-9]+.bitcask.data$", false,
-                       fun(F, Acc) ->
-                               [{file_tstamp(F), F} | Acc]
-                       end, []).
-
+    case list_dir(Dirname) of
+        {ok, Files} ->
+            lists:foldl(
+              fun(Filename, Acc) ->
+                      case string:tokens(Filename, ".") of
+                          [TSString, _, "data"] ->
+                              [{list_to_integer(TSString), 
+                                filename:join(Dirname, Filename)}
+                                | Acc];
+                          _ ->
+                              Acc
+                      end
+              end,
+              [], Files);
+        {error, Reason} ->
+            {error, Reason}
+    end.
 
 %% @doc Use only after merging, to permanently delete a data file.
 -spec delete(#filestate{}) -> ok | {error, atom()}.
@@ -299,7 +341,7 @@ check_write(#filestate { ofs = Offset }, Key, Value, MaxSize) ->
     end.
 
 has_hintfile(#filestate { filename = Fname }) ->
-    filelib:is_file(hintfile_name(Fname)).
+    is_file(hintfile_name(Fname)).
 
 %% Return true if there is a hintfile and it has
 %% a valid CRC check
@@ -309,7 +351,7 @@ has_valid_hintfile(State) ->
             HintFile = hintfile_name(State),
             case bitcask_io:file_open(HintFile, [readonly, read_ahead]) of
                 {ok, HintFd} -> 
-                    {ok, HintI} = file:read_file_info(HintFile),
+                    {ok, HintI} = read_file_info(HintFile),
                     HintSize = HintI#file_info.size,
                     hintfile_validate_loop(HintFd, 0, HintSize);
                 _ ->
@@ -455,7 +497,7 @@ fold_hintfile(State, Fun, Acc0) ->
     case bitcask_io:file_open(HintFile, [readonly, read_ahead]) of
         {ok, HintFd} ->
             try
-                {ok, DataI} = file:read_file_info(State#filestate.filename),
+                {ok, DataI} = read_file_info(State#filestate.filename),
                 DataSize = DataI#file_info.size,
                 case fold_file_loop(HintFd, fun fold_hintfile_loop/6, Fun, Acc0, 
                                     {DataSize, HintFile}) of 
@@ -611,39 +653,18 @@ fold_file_loop(Fd, FoldFn, IntFoldFn, Acc0, Args0, Prev0, ChunkSz0) ->
             {error, Reason}
     end.
 
-create_file_loop(DirName, Opts, Tstamp) ->
-    Filename = mk_filename(DirName, Tstamp),
-    ok = filelib:ensure_dir(Filename),
-
-    %% Check for o_sync strategy and add to opts
-    FinalOpts = case bitcask:get_opt(sync_strategy, Opts) of
-                    o_sync ->
-                        [o_sync | Opts];
-                    _ ->
-                        Opts
-                end,
-
-    case bitcask_io:file_open(Filename, FinalOpts) of
+open_hint_file(Filename, FinalOpts) ->
+    open_hint_file(Filename, FinalOpts, 10).
+    
+open_hint_file(_Filename, _FinalOpts, 0) ->
+    throw(couldnt_open_hintfile);
+open_hint_file(Filename, FinalOpts, Count) ->
+    case bitcask_io:file_open(hintfile_name(Filename), FinalOpts) of 
         {ok, FD} ->
-            {ok, HintFD} = bitcask_io:file_open(hintfile_name(Filename), FinalOpts),
-            {ok, #filestate{mode = read_write,
-                            filename = Filename,
-                            tstamp = file_tstamp(Filename),
-                            hintfd = HintFD, fd = FD, ofs = 0}};
-
-        {error, _} ->
-            %% Couldn't create a new file with the requested name,
-            %% check for the more recent timestamp and increment by
-            %% 1 and try again.
-            %% This introduces some drift into the actual creation
-            %% time, but given that we only have at most 2 writers
-            %% (writer + merger) for a given bitcask, it shouldn't be
-            %% more than a few seconds. The alternative it to sleep
-            %% until the next second rolls around -- but this
-            %% introduces lengthy, unnecessary delays.
-            %% This also handles the possibility of clock skew
-            MostRecentTS = most_recent_tstamp(DirName),
-            create_file_loop(DirName, Opts, MostRecentTS + 1)
+            FD;
+        {error, eexist} -> 
+            timer:sleep(50),
+            open_hint_file(Filename, FinalOpts, Count - 1)
     end.
 
 hintfile_entry(Key, Tstamp, {Offset, TotalSz}) ->
@@ -651,8 +672,88 @@ hintfile_entry(Key, Tstamp, {Offset, TotalSz}) ->
     [<<Tstamp:?TSTAMPFIELD>>, <<KeySz:?KEYSIZEFIELD>>,
      <<TotalSz:?TOTALSIZEFIELD>>, <<Offset:?OFFSETFIELD>>, Key].
 
-%% Find the most recent timestamp for a .data file
-most_recent_tstamp(DirName) ->
-    lists:foldl(fun({TS,_},Acc) -> 
-                       erlang:max(TS, Acc)
-               end, 0, data_file_tstamps(DirName)).
+%% ===================================================================
+%% file/filelib avoidance code.
+%% ===================================================================
+
+read_file_info(FileName) ->
+    prim_file:read_file_info(FileName).
+
+write_file_info(FileName, Info) ->
+    prim_file:write_file_info(FileName, Info).
+
+is_file(File) ->
+    case read_file_info(File) of
+        {ok, #file_info{type=regular}} ->
+            true;
+        {ok, #file_info{type=directory}} ->
+            true;
+        _ ->
+            false
+    end.
+
+is_dir(File) ->
+    case read_file_info(File) of
+        {ok, #file_info{type=directory}} ->
+            true;
+        _ ->
+            false
+    end.
+
+ensure_dir("/") ->
+    ok;
+ensure_dir(F) ->
+    Dir = filename:dirname(F),
+    case is_dir(Dir) of
+        true ->
+            ok;
+        false when Dir =:= F ->
+            %% Protect against infinite loop
+            {error,einval};
+        false ->
+            ensure_dir(Dir),
+            %% this is rare enough that the serialization 
+            %% isn't super important, maybe
+            case file:make_dir(Dir) of
+                {error,eexist}=EExist ->
+                    case is_dir(Dir) of
+                        true ->
+                            ok;
+                        false ->
+                            EExist
+                    end;
+                Err ->
+                    Err
+            end
+    end.
+
+list_dir(Directory) ->
+    Port = get_efile_port(),
+    prim_file:list_dir(Port, Directory).
+
+get_efile_port() ->
+    Key = bitcask_efile_port,
+    case get(Key) of
+        undefined ->
+            case prim_file_drv_open(efile, [binary]) of
+                {ok, Port} ->
+                    put(Key, Port),
+                    get_efile_port();
+                Err ->
+                    error_logger:error_msg("get_efile_port: ~p\n", [Err]),
+                    timer:sleep(1000),
+                    get_efile_port()
+            end;
+        Port ->
+            Port
+    end.
+
+prim_file_drv_open(Driver, Portopts) ->
+    try erlang:open_port({spawn, Driver}, Portopts) of
+        Port ->
+            {ok, Port}
+    catch
+        error:Reason ->
+            {error, Reason}
+    end.
+

c_src/bitcask_nifs.c
@@ -202,6 +202,8 @@ ERL_NIF_TERM bitcask_nifs_keydir_info(ErlNifEnv* env, int argc, const ERL_NIF_TE
 ERL_NIF_TERM bitcask_nifs_keydir_release(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 ERL_NIF_TERM bitcask_nifs_keydir_trim_fstats(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 
+ERL_NIF_TERM bitcask_nifs_increment_file_id(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
+
 ERL_NIF_TERM bitcask_nifs_create_tmp_file(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
 
 ERL_NIF_TERM bitcask_nifs_lock_acquire(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[]);
@@ -247,6 +249,8 @@ static ErlNifFunc nif_funcs[] =
     {"keydir_info", 1, bitcask_nifs_keydir_info},
     {"keydir_release", 1, bitcask_nifs_keydir_release},
     {"keydir_trim_fstats", 2, bitcask_nifs_keydir_trim_fstats},
+
+    {"increment_file_id", 1, bitcask_nifs_increment_file_id},
 
     {"lock_acquire_int",   2, bitcask_nifs_lock_acquire},
     {"lock_release_int",   1, bitcask_nifs_lock_release},
@@ -1192,6 +1196,25 @@ ERL_NIF_TERM bitcask_nifs_keydir_release(ErlNifEnv* env, int argc, const ERL_NIF
     }
 }
 
+ERL_NIF_TERM bitcask_nifs_increment_file_id(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
+{
+    bitcask_keydir_handle* handle;
+
+    if (enif_get_resource(env, argv[0], bitcask_keydir_RESOURCE, (void**)&handle))
+    {
+
+        LOCK(handle->keydir);
+        (handle->keydir->biggest_file_id)++;
+        uint32_t id = handle->keydir->biggest_file_id;
+        UNLOCK(handle->keydir);
+        return enif_make_tuple2(env, ATOM_OK, enif_make_uint(env, id));
+    }
+    else
+    {
+        return enif_make_badarg(env);
+    }
+}
+
 ERL_NIF_TERM bitcask_nifs_keydir_trim_fstats(ErlNifEnv* env, int argc, const ERL_NIF_TERM argv[])
 {
     bitcask_keydir_handle* handle;
@@ -1217,7 +1240,7 @@ ERL_NIF_TERM bitcask_nifs_keydir_trim_fstats(ErlNifEnv* env, int argc, const ERL
             {
                 bitcask_fstats_entry* curr_f;
                 curr_f = kh_val(keydir->fstats, itr);
-                free(curr_f);
+                enif_free_compat(env, curr_f);
                 kh_del(fstats, keydir->fstats, itr);
             }
             else

src/bitcask_lockops.erl
@@ -31,7 +31,7 @@
 -compile({parse_transform, pulse_instrument}).
 -endif.
 
--type lock_types() :: merge | write.
+-type lock_types() :: merge | write | create.
 
 %% @doc Attempt to lock the specified directory with a specific type of lock
 %% (merge or write).

src/bitcask_nifs.erl
@@ -38,6 +38,7 @@
          keydir_wait_pending/1,
          keydir_info/1,
          keydir_release/1,
+         increment_file_id/1,
          keydir_trim_fstats/2,
          lock_acquire/2,
          lock_release/1,
@@ -120,7 +121,7 @@
 -spec keydir_release(reference()) ->
         ok.
 -spec keydir_trim_fstats(reference(), [integer()]) ->
-        ok.
+        {ok, integer()} | {error, atom()}.
 -spec lock_acquire(string(), integer()) ->
         {ok, reference()} | {error, atom()}.
 -spec lock_release(reference()) ->
@@ -240,6 +241,10 @@ keydir_itr_next_int(_Ref) ->
 
 keydir_itr_release(_Ref) ->
     ok.
+
+
+increment_file_id(_Ref) ->
+    erlang:nif_error({error, not_loaded}).
 
 keydir_fold(Ref, Fun, Acc0, MaxAge, MaxPuts) ->
     FrozenFun = fun() -> keydir_fold_cont(keydir_itr_next(Ref), Ref, Fun, Acc0) end,

commit fb5e962542cbfa6cd5a05f6c2837e917b0f1c7dc

- start with scott's commit 0d3acb0 - fix the issue introduced in 6c97173 more mininally - ifdef out env reading even though it doesn't matter

Makefile
@@ -23,10 +23,10 @@ clean:
 test: deps compile eunit_erlang eunit_nif
 
 eunit_erlang:
-	IOMODE="erlang" $(REBAR_BIN) skip_deps=true eunit
+	BITCASK_IO_MODE="erlang" $(REBAR_BIN) skip_deps=true eunit
 
 eunit_nif:
-	IOMODE="nif" $(REBAR_BIN) skip_deps=true eunit
+	BITCASK_IO_MODE="nif" $(REBAR_BIN) skip_deps=true eunit
 
 
 # Release tarball creation

src/bitcask.erl
@@ -1386,17 +1386,11 @@ default_dataset() ->
 %% HACK: Terrible hack to ensure that the .app file for
 %% bitcask is available on the code path. Assumption here
 %% is that we're running in .eunit/ as part of rebar.
-%% MORE HACK: init the io mode (nif or erlang) here.
 a0_test() ->
     code:add_pathz("../ebin"),
     application:start(erlang),
-    Mode0 = case os:getenv("IOMODE") of
-                false -> "erlang";
-                Else -> Else
-            end,
-    Mode = list_to_atom(Mode0),
-    application:set_env(bitcask, io_mode, Mode),
-    error_logger:info_msg("Set IO mode to: ~p", [Mode]).
+    Mode = bitcask_io:determine_file_module(),
+    error_logger:info_msg("Bitcask IO mode is: ~p\n", [Mode]).
 
 roundtrip_test() ->
     os:cmd("rm -rf /tmp/bc.test.roundtrip"),

src/bitcask_fileops.erl
@@ -65,7 +65,9 @@
 -spec create_file(Dirname :: string(), Opts :: [any()],
                   reference()) -> 
                          {ok, #filestate{}}.
-create_file(DirName, Opts, Keydir) ->
+
+create_file(DirName, Opts0, Keydir) ->
+    Opts = [create|Opts0],
     {ok, Lock} = get_create_lock(DirName),
     try 
         {ok, Newest} = bitcask_nifs:increment_file_id(Keydir),

src/bitcask_io.erl
@@ -68,6 +68,23 @@ file_module() ->
             Mod
     end.
 
+-ifdef(TEST).
+determine_file_module() ->
+    case application:get_env(bitcask, io_mode) of
+        {ok, erlang} ->
+            bitcask_file;
+        {ok, nif} ->
+            bitcask_nifs;
+        _ ->
+            Mode = case os:getenv("BITCASK_IO_MODE") of
+                       false    -> 'erlang';
+                       "erlang" -> 'erlang';
+                       "nif"    -> 'nif'
+                    end,
+            application:set_env(bitcask, io_mode, Mode),
+            determine_file_module()
+    end.
+-else.
 determine_file_module() ->
     case application:get_env(bitcask, io_mode) of
         {ok, erlang} ->
@@ -77,3 +94,4 @@ determine_file_module() ->
         _ ->
             bitcask_file
     end.
+-endif.